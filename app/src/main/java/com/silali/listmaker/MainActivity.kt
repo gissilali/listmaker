f921d753beb0c1c950e836bd18d9e2de5b668ddc
silali <gibsonsilali@gmail.com>
2020-07-28 15:22:00 +0300

    cached credential test

--- app/src/main/java/com/silali/listmaker/MainActivity.kt
+++ app/src/main/java/com/silali/listmaker/MainActivity.kt
@@ -26,7 +26,6 @@ class MainActivity : AppCompatActivity() {
             val adapter = todoListRecyclerView.adapter as TodoListAdapter
             adapter.addNewItem()
         }
-
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
